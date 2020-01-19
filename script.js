a21dd162aac46c63fc5eae64706fb7e3fae0ef78
Kendall Arceo <arceokendall@gmail.com>
2020-01-19 11:41:34 -0600

    Added CSS to visualize where questions, buttons, and positioning will be. Also added variables for the javascript portion.

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // Questions variables
-var 
+// var maint content
+// var quiz content (switching it from the main to the quiz to start)
+// Use append child and replace child to switch
+
+var currentQuestion = 0;
+var score = 0;
+var toQuestions = questions.length;
+
+var container = document.getElementById("quizContainer");
+var questionsEl = document.getElementById("question");
+
+var opt1 = document.getElementById("opt1")
+var opt2 = document.getElementById("opt2")
+var opt3 = document.getElementById("opt3")
+var opt4 = document.getElementById("opt4")
+var nextButton = document.getElementById("nextButton");
+var resultCont = document.getElementById("result");
