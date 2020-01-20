998ea18d2dd5f7da4b5b8ffd731ee53c7ead7b18
Kendall Arceo <arceokendall@gmail.com>
2020-01-19 18:00:45 -0600

    Questions loaded into js. results, timer, and div transition needed to complete. Finalize CSS later, CSS right now is to visualize.

---
+++ questions.js
@@ -0,0 +1,46 @@
+var questions = [{
+    "question": "Commonly used data types DO NOT include:",
+    "option1": "Strings",
+    "option2": "Booleans",
+    "option3": "Alerts",
+    "option4": "Numbers",
+    "answer": "3"
+
+
+}, {
+    "question": "The condition in an if/else statement is enclosed within",
+    "option1": "Quotes",
+    "option2": "Curly Brackets",
+    "option3": "Parenthesis",
+    "option4": "Square Brackets",
+    "answer": "2"
+
+
+},{
+    "question": "Arrays in Javascript can be used to store",
+    "option1": "Numbers and strings",
+    "option2": "Other Arrays",
+    "option3": "Booleans",
+    "option4": "All of the above",
+    "answer": "4"
+
+
+},{
+    "question": "String values must be enclosed within____. When being assigned to variables",
+    "option1": "Commas",
+    "option2": "Curly Brackets",
+    "option3": "Quotes",
+    "option4": "Parenthesis",
+    "answer": "4"
+
+
+},{
+    "question": "A very useful tool used during development and debugginf for printing content to the debugger is",
+    "option1": "Javascript",
+    "option2": "Terminal/Bash",
+    "option3": "For Loops",
+    "option4": "Console.log",
+    "answer": "4"
+
+
+}]
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Questions variables
-// var maint content
+// var main content
 // var quiz content (switching it from the main to the quiz to start)
 // Use append child and replace child to switch
 
 var currentQuestion = 0;
 var score = 0;
-var toQuestions = questions.length;
+var totQuestions = questions.length;
 
+var startContainer = document.getElementById("startContainer");
 var container = document.getElementById("quizContainer");
 var questionsEl = document.getElementById("question");
 
@@ -16,3 +17,55 @@ var opt3 = document.getElementById("opt3")
 var opt4 = document.getElementById("opt4")
 var nextButton = document.getElementById("nextButton");
 var resultCont = document.getElementById("result");
+
+var x = document.getElementById("quizContainer").nextSibling.innerHTML;
+
+
+
+function loadQuestion (questionIndex) {
+    var q = questions[questionIndex];
+    questionsEl.textContent = (questionIndex + 1) + ". " + q.question;
+    opt1.textContent = q.option1;
+    opt2.textContent = q.option2;
+    opt3.textContent = q.option3;
+    opt4.textContent = q.option4;
+
+};
+
+function loadNextQuestion () {
+    var selectedOption = document.querySelector("input[type=radio]:checked");
+    if (!selectedOption) {
+        alert("Select your answer!");
+        return;
+
+    }
+
+    var answer = selectedOption.nodeValue;
+    if(questions[currentQuestion].answer == answer) {
+        score += 20;
+
+    }
+
+    selectedOption.checked = false;
+    currentQuestion++;
+    if(currentQuestion == totQuestions - 1) {
+        nextButton.textContent = "Finish";
+
+    }
+     
+    //result container transition
+    if(currentQuestion == totQuestions) {
+        container.style.display = "none" ;
+        resultCont.style.display = " ";
+        resultCont.textContent = "Your score is" + score;
+        return;
+
+
+    }
+
+    loadQuestion(currentQuestion);
+
+
+}
+
+loadQuestion(currentQuestion)
